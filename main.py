698ef5066e7beffc3affe3208001477ea1e803e8
maneeshsandra <57929979+maneeshsandra@users.noreply.github.com>
2021-06-23 22:54:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 previous=" "
 while(1):
     url="https://www.espncricinfo.com/series/icc-world-test-championship-2019-2021-1195334/india-vs-new-zealand-final-1249875/live-cricket-score"
+    # replace this URL with present match url
     cricInfo=requests.get(url).text
     soup=BeautifulSoup(cricInfo,'lxml')
     comm=soup.find('div',{"class":"d-flex match-comment-padder align-items-center"})
